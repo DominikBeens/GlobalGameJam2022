750b0345dc31fec486d8c753d5cb7fed657c122c
Dominik Beens <dominik.beens@gmail.com>
2022-01-29 17:50:11 +0100

    projectiles now affect gates

--- Assets/Scripts/Game/Obstacles/Obstacle.cs
+++ Assets/Scripts/Game/Obstacles/Obstacle.cs
@@ -1,10 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class Obstacle : MonoBehaviour {
 
-    public void Intialize() {
+    public Visual ActiveVisual { get; private set; }
 
+    [SerializeField] private List<Visual> visuals = new();
+
+    [Serializable]
+    public struct Visual {
+        public VisualType VisualType;
+        public GameObject GameObject;
+    }
+
+    public void Intialize() {
+        VisualType random = Random.value < 0.5f ? VisualType.Light : VisualType.Dark;
+        SetVisual(random);
+    }
+
+    public void Hit(VisualType visualType) {
+        if (visualType == ActiveVisual.VisualType) { return; }
+        SetVisual(visualType);
+        Animate();
+    }
+
+    private void SetVisual(VisualType visualType) {
+        if (ActiveVisual.GameObject) {
+            ActiveVisual.GameObject.transform.DOKill(true);
+        }
+
+        ActiveVisual = visuals.Find(x => x.VisualType == visualType);
+        foreach (Visual visual in visuals) {
+            visual.GameObject.SetActive(visual.VisualType == visualType);
+        }
+    }
+
+    private void Animate() {
+        ActiveVisual.GameObject.transform.DOPunchScale(new Vector3(0.15f, 0.25f, 0.15f), 0.25f);
     }
 }
--- Assets/Scripts/Game/PlayerProjectile.cs
+++ Assets/Scripts/Game/PlayerProjectile.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerProjectile : MonoBehaviour {
 
     [SerializeField] private Transform visual;
+    [SerializeField] private VisualType visualType;
+    [Space]
     [SerializeField] private float force = 5f;
+    [SerializeField] private float lifetime = 5f;
 
     private Rigidbody2D body;
+    private bool isDestroying;
+    private float remainingLifetime;
 
     public void Initialize(Vector3 direction) {
         body = GetComponent<Rigidbody2D>();
+        remainingLifetime = lifetime;
 
         Vector2 scaledForce = direction * force * Time.deltaTime;
         body.AddForce(scaledForce, ForceMode2D.Impulse);
+
+        visual.localScale = Vector3.zero;
+        visual.DOScale(1f, 0.3f).SetEase(Ease.OutBack);
+    }
+
+    private void Update() {
+        ProcessLifetime();
+    }
+
+    private void ProcessLifetime() {
+        if (isDestroying) { return; }
+
+        remainingLifetime -= Time.deltaTime;
+        if (remainingLifetime > 0f) { return; }
+
+        Destroy();
+    }
+
+    private void Destroy() {
+        isDestroying = true;
+        visual.DOKill();
+        visual.DOScale(0f, 0.2f).OnComplete(() => Destroy(gameObject));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (isDestroying) { return; }
+
+        Obstacle obstacle = collider.GetComponent<Obstacle>();
+        if (obstacle) {
+            obstacle.Hit(visualType);
+            Destroy();
+        }
     }
 }
